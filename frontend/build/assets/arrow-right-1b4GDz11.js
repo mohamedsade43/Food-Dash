93de5d8740adbfb40aa98e9881b18ed4b46de4c6
mohamedsade43@gmail.com <mohamedsade43@gmail.com>
2024-06-23 07:17:34 +0300

    completed all remaining parts

--- frontend/build/assets/arrow-right-1b4GDz11.js
+++
@@ -1,6 +0,0 @@
-import{m as r}from"./index-V3xU5zvR.js";/**
- * @license lucide-react v0.390.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=r("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);export{o as A};
